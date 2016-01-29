74d47b5c2677bc08919c41811de50157e9aae320
wutongr <zc_and_zc@aliyun.com>
2016-01-30 00:44:59 +0800

    Initial Commit

---
+++ WTLoading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTLoading
+//
+//  Created by Yorke on 16/1/30.
+//  Copyright © 2016年 WuTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTLoading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTLoading
+//
+//  Created by Yorke on 16/1/30.
+//  Copyright © 2016年 WuTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
